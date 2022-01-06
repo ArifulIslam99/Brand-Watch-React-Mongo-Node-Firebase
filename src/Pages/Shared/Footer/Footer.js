db1f485efd9bc92487acdec6869f83027ba4f0a3
ArifulIslam99 <afridi2000rampura@gmail.com>
2022-01-06 19:11:02 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Footer.css'
+import logo from '../../../images/logo_TW.png'
+
 const Footer = () => {
     return (
-        <div style={{width:'auto', backgroundColor:'black', color:'gray'}}>
-            <footer className="py-2">Copyright © 2021  Times World</footer>
+        <div className="footer-all w-100 p-3">
+            <div className="footer row ">
+            <div className="col-lg-3 col-xs-12 my-auto py-3">
+               
+               <img  className='fluid w-100' src={logo} alt="" />
+                
+            </div> 
+            <div className="col-lg-3 col-xs-12 my-auto py-3 ">
+                <p>Looking for a new watch ? </p>
+                <Link to="/explore"><Button variant="success">Explore</Button></Link>
+            </div>
+            <div className="col-lg-3 col-xs-12 my-auto py-3">
+                <h3>Quick Visit</h3>
+                <p><Link className="url"  to="/home">Home   </Link></p>
+                <p><Link className="url"  to="/explore">Products </Link></p>
+                
+
+            </div> 
+            <div className="col-lg-3 col-xs-12 my-auto py-3">
+                <h3>Contact Us</h3>
+                <p>Email: admin@timesworld.com</p> 
+                <a target='blank' href='https://www.facebook.com/'>
+                 <span className='fs-4 p-2'> <i className="fab fa-facebook"></i></span>
+                </a> 
+                 
+                <a target='blank' href='https://www.instagram.com/'>
+                <span className='fs-4 p-2'><i className="fab fa-instagram"></i></span>
+                </a> 
+
+                <a target='blank' href='https://www.linkedin.com/'>
+                <span className='fs-4 p-2'><i className="fab fa-linkedin"></i></span>
+                </a> 
+              
+            </div>
+        </div>
+        <div>
+            <footer style={{fontWeight:'600', color:'orangered'}}> Copyright 2022 © Times World</footer>
+        </div>
         </div>
     );
 };
